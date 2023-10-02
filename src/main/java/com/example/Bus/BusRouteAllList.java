5b4def475b74c7a6cce855dd18f27b8722ea8a2c
JEONG Cheolwoo <tonyprince1228@gmail.com>
2023-10-03 00:45:16 +0900

    .

--- src/main/java/com/example/Bus/BusRouteAllList.java
+++ src/main/java/com/example/Bus/BusRouteAllList.java
@@ -3,6 +3,10 @@ package com.example.Bus;
 // 경유노선 전체 정류소 도착예정정보를 조회한다
 // 변수: BusRouteId(노선 ID)
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -10,8 +14,10 @@ import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class getArrInfoByRouteAllList {
-    public getArrInfoByRouteAllList(String busRouteId) throws IOException {
+@RestController
+public class BusRouteAllList {
+    @GetMapping("/getArrInfoByRouteAll")
+    public void getArrInfoByRouteAllList(@RequestParam String busRouteId) throws IOException {
         StringBuilder urlBuilder = new StringBuilder("http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=t2qs2a1o15tXR1NhKWY%2FTplsMnvey2e3kTFt8BIlR8dJ6JsaALNvYI6%2B5dKPSJbl%2FJ9C0dF7%2Boi2NwGJKHikSQ%3D%3D"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("busRouteId","UTF-8") + "=" + URLEncoder.encode(busRouteId, "UTF-8")); /*노선ID*/
@@ -24,7 +30,10 @@ public class getArrInfoByRouteAllList {
         BufferedReader rd;
 
         if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            System.out.println("!@@!@!@!");
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            // XML 데이터 파싱 및 출력
+            XmlParsing_test.getData();
         } else {
             rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
