91266bbc61aa170f7577afdcbd1c5064bd62bd2c
Yu-Jin9 <ujh50188@gmail.com>
2023-10-02 21:33:57 +0900

    driver_List
    List오류 고침

--- 새 폴더/Components/Catch.js
+++ 새 폴더/Components/Catch.js
@@ -1,57 +1,32 @@
-// 버스 등록 페이지 -> 버스 고유 번호 확인 페이지 ( 몇분 뒤 도착하는 버스 탈건지 )
-import { FlatList } from 'react-native';
-import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, SafeAreaView } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
 
-// 버스 고유번호
-const busUids = [
-    { id: 1, title: '1234' },
-    { id: 2, title: '전화ㅈ봉고' },
-    { id: 3, title: '울 팀고 띠팅 나가랑 ㅜㄴ구 본적일ㄴ느'},
-    { id: 4, title: '5678' },
-    // ... 추가 아이템들
-  ];
+const RideNotifications = () => {
+  const [rideData, setRideData] = useState([]);
 
+  // 사용자가 보낸 승차 알림을 처리하는 함수
+  const handleRideNotification = (busNumber, userName, busStop) => {
+    // 새로운 알림을 목록에 추가
+    const newRide = { busNumber, userName, busStop };
+    setRideData([...rideData, newRide]);
+  };
 
-function Catch ({navigation, route}) {
- //   const { itemId, itemTitle } = route.params;
-    // 특정 버스 눌렀을 때 
-    const handleItemPress = (item) => {
-        navigation.navigate('CheckRideBus', {
-          itemId: item.id,
-          itemTitle: item.title,
-          //na.jape.dieee
-        });
-      };
-    return (
-        <View>
-            <FlatListvaluse
-            data={busUids}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => handleItemPress(item)}>
-                    <ListItem bottomDivider>
-                    <ListItem.Content>
-                        <ListItem.Title>{item.title}</ListItem.Title>
-                    </ListItem.Content>
-                    </ListItem>
-                </TouchableOpacity>
-            )}
-        />
-        </View>
-        
-    );
+  return (
+    <View>
+      <Text>승차 알림 목록</Text>
+      <FlatList
+        data={rideData}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>버스 번호: {item.busNumber}</Text>
+            <Text>사용자 이름: {item.userName}</Text>
+            <Text>정류장 이름: {item.busStop}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-    titleStyle: {
-      fontSize: 24, 
-      fontWeight: 'bold', 
-      marginVertical: 10
-    },
-    
-});
-  
-
-export default Catch;
+export default RideNotifications;
