ae91988f7dc8eff8f83394790c7642e28e57fb9e
JEONG Cheolwoo <tonyprince1228@gmail.com>
2023-10-10 16:03:02 +0900

    commit

--- src/main/java/com/example/Bus/BusApplication.java
+++ src/main/java/com/example/Bus/BusApplication.java
@@ -2,12 +2,17 @@ package com.example.Bus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BusApplication {
+public class BusApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(BusApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BusApplication.class);
+	}
 }
